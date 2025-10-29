1aa1ec9ee89e44a93333987b897e4387784c454d
Julio Dias <juliocesarddn@gmail.com>
2025-10-28 22:31:24 -0300

    Primeiro commit

---
+++ src/OlaMergulhador.java
@@ -0,0 +1,9 @@
+public class OlaMergulhador {
+
+    public static void main(String[] args) {
+        //Ponto de entrada do programa.
+        System.out.println("Olá mergulhador!");
+
+        // O uso do ; é obrigatório para fechar toda instrução
+    }
+}
