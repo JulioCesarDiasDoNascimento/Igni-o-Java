3cb0e806049cf4a8432fb1a3be52b8a42439210e
Julio Dias <juliocesarddn@gmail.com>
2025-10-29 01:34:04 -0300

    1.4 Métodos, construtores e sobrecarga

--- src/Conta.java
+++ src/Conta.java
@@ -1,7 +1,41 @@
+import java.util.Objects;
+
 public class Conta {
 
     Pessoa titular; // Se chama relação tem um Ex: Conta tem um Pessoa
     int agencia;
     int numero;
     double saldo;
+
+    Conta (Pessoa titular, int agencia, int numero) {
+        Objects.requireNonNull(titular); // Isso é praticamente um if!!! Ótima pratica
+        this.titular = titular;
+        this.agencia = agencia;
+        this.numero = numero;
+    }
+
+    Conta () {}
+
+    void depositar(double valor){
+
+        if(valor <= 0){
+            throw new IllegalArgumentException("Valor deve ser maior que zero");
+        }
+        saldo += valor;
+    }
+    void sacar(double valor) {
+        if(valor <= 0){
+            throw new IllegalArgumentException("Valor deve ser maior que zero");
+        }
+
+        if(saldo - valor < 0) {
+            throw new IllegalStateException("Saldo insuficiente");
+        } ;
+        saldo -= valor;
+    }
+
+    void sacar (double valor, double taxaSaque) {
+        sacar(valor + taxaSaque);
+
+    }
 }
--- src/Principal.java
+++ src/Principal.java
@@ -11,12 +11,13 @@ public class Principal {
         voce.documento = "987.654.321-00";
 
         //Estou criando um novo objeto com o molde da classe conta
-        Conta minhaConta = new Conta();
-        minhaConta.titular = eu; // sempre lembrar que titular agora é um objeto da classe pessoa e precisa ser instanciado.
-        minhaConta.agencia = 6250;
-        minhaConta.numero = 17854;
+        Conta minhaConta = new Conta(eu, 6520, 17587);
+
         minhaConta.saldo = 15000.0d;
+        minhaConta.depositar(1000.0d);
+        minhaConta.sacar(5000.0d, 10);
         System.out.println("Titular: " + minhaConta.titular.nome);
+        System.out.println("Saldo: " + minhaConta.saldo);
 
 
         Conta suaConta = new Conta();
@@ -24,7 +25,10 @@ public class Principal {
         suaConta.agencia = 6250;
         suaConta.numero = 17853;
         suaConta.saldo = 3000.0d;
+        suaConta.depositar(1000.0d);
+        suaConta.sacar(4000.0d);
         System.out.println("Titular: " + suaConta.titular.nome);
+        System.out.println("Saldo: " + suaConta.saldo);
 
         // fiz dois objetos baseado no mesmo molde(SÂO OBJETOS DIFERENTES)
 
