ec5f2813c8c95c4d6ac3330fb012d6d69bd97fb6
Julio Dias <juliocesarddn@gmail.com>
2025-11-01 11:40:35 -0300

    2.2 Upcasting e polimorfismo

--- src/com/simpli/financial/app/Principal.java
+++ src/com/simpli/financial/app/Principal.java
@@ -1,9 +1,6 @@
 package com.simpli.financial.app;
 
-import com.simpli.financial.modelo.Conta;
-import com.simpli.financial.modelo.ContaEspecial;
-import com.simpli.financial.modelo.ContaInvestimento;
-import com.simpli.financial.modelo.Pessoa;
+import com.simpli.financial.modelo.*;
 
 public class Principal {
 
@@ -17,23 +14,22 @@ public class Principal {
         voce.setNome("Giovanna Penteado");
         voce.setDocumento("987.654.321-00");
 
+        CaixaEletronico caixa = new CaixaEletronico();
         //Estou a criar um objeto com o molde da classe conta
         ContaInvestimento minhaConta = new ContaInvestimento(eu, 6520, 17587);
 
         minhaConta.depositar(1000.0d);
         minhaConta.sacar(500.0d, 10);
         minhaConta.creditarRendimentos(0.8d);
-        System.out.println("Titular: " + minhaConta.getTitular().getNome());
-        System.out.println("Saldo: " + minhaConta.getSaldo());
+        caixa.imprimirSaldo(minhaConta);
 
 
         ContaEspecial suaConta = new ContaEspecial(voce,6250,62814,1000.0d);
         suaConta.depositar(1000.0d);
         suaConta.sacar(1500.0);
-        System.out.println("Titular: " + suaConta.getTitular().getNome());
-        System.out.println("Saldo: " + suaConta.getSaldoDisponivel());
+        caixa.imprimirSaldo(suaConta);
 
-        // fiz dois objetos baseado no mesmo molde(SÂO OBJETOS DIFERENTES)
+        // Fiz dois objetos baseado no mesmo molde(SÂO OBJETOS DIFERENTES)
 
     }
 }
